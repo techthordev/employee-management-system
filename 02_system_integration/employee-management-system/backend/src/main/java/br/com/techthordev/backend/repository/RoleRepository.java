e4c24d1be2f754b769c5a3395b4de3a7a546ea04
techthordev <thorsten.fey@outlook.com>
2026-02-21 16:20:17 -0300

    fix: added RoleRepository

---
+++ 02_system_integration/employee-management-system/backend/src/main/java/br/com/techthordev/backend/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package br.com.techthordev.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.techthordev.backend.entity.Role;
+import java.util.Optional;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+}
